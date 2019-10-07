68bfd9e76f80a35300ea3fb50e495226695dcb12
edadasko <edadasko@gmail.com>
2019-10-07 22:19:28 +0300

    Add showing current version name

--- lab1_HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ lab1_HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import android.content.pm.ActivityInfo;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayVersionNum();
+    }
+
+    private void displayVersionNum() {
+        TextView versionNum = findViewById(R.id.version_num);
+        versionNum.append(" " + BuildConfig.VERSION_NAME);
     }
 }
